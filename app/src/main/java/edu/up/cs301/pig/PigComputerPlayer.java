a23da574e0d7282329a7e94e795bd53fd403ed5e
Zhi Zun Chen <beurryllmew@gmail.com>
2020-02-24 20:39:16 -0800

    Finished Computer Pigger

--- app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
@@ -1,9 +1,13 @@
 package edu.up.cs301.pig;
 
+import edu.up.cs301.game.GameFramework.Game;
 import edu.up.cs301.game.GameFramework.GameComputerPlayer;
+import edu.up.cs301.game.GameFramework.LocalGame;
 import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
 import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
+import edu.up.cs301.game.GameFramework.infoMessage.GameState;
 import edu.up.cs301.game.GameFramework.utilities.Tickable;
+import java.util.*;
 
 /**
  * An AI for Pig
@@ -28,7 +32,20 @@ public class PigComputerPlayer extends GameComputerPlayer {
      */
     @Override
     protected void receiveInfo(GameInfo info) {
-        // TODO  You will implement this method
+        PigGameState copy = new PigGameState((PigGameState) info);
+        if(copy.getTurn() != super.playerNum) {
+            return;
+        } else {
+            Random rand = new Random();
+            int choice = rand.nextInt(2);
+            if(choice == 0) {
+                GameAction action = new PigHoldAction(this);
+                game.sendAction(action);
+            } else {
+                GameAction action = new PigRollAction(this);
+                game.sendAction(action);
+            }
+        }
     }//receiveInfo
 
 }
